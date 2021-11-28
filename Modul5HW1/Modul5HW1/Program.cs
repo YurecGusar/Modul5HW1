a5f7365788d4db5e57ad35a24b4a53f647c3720d
DESKTOP-HO5O0U5\leoon <leoonovn@gmail.com>
2021-11-28 14:37:29 +0200

    master

---
+++ Modul5HW1/Modul5HW1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Modul5HW1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
